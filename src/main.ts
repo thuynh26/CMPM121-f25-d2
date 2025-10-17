f8a5809aabceb4465335d7c1b5914302ee1788fb
thuynh26 <thuynh26@ucsc.edu>
2025-10-17 21:57:35 +0000

    Implemented drawing and clear btn (Step 2 Complete)

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,39 @@ document.body.innerHTML = `
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
-canvas.style.cursor = "none";
 document.body.append(canvas);
+
+const ctx = canvas.getContext("2d")!;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (_e) => {
+  cursor.active = false;
+});
+
+document.body.append(document.createElement("br"));
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
