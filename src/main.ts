27367d229f0eb387ae8f5d7fdc67a42721fb20a7
thuynh26 <thuynh26@ucsc.edu>
2025-10-17 21:23:45 +0000

    Added canvas element with border and shadow (Step 1 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,11 @@
 import "./style.css";
 
 document.body.innerHTML = `
-  <h1>Heading</h1>
-  <canvas id="myCanvas" width="256" height="256"></canvas>
-
+  <h1>Temp Heading</h1>
 `;
 
-const canvas = document.getElementById("myCanvas")! as HTMLCanvasElement;
-
-const ctx = canvas.getContext("2d");
-
-const cursor = { active: false, x: 0, y: 0 };
-
-canvas.addEventListener("mousedown", (e) => {
-  cursor.active = true;
-  cursor.x = e.offsetX;
-  cursor.y = e.offsetY;
-});
-
-canvas.addEventListener("mousemove", (e) => {
-  if (cursor.active) {
-    ctx!.beginPath();
-    ctx!.moveTo(cursor.x, cursor.y);
-    ctx!.lineTo(e.offsetX, e.offsetY);
-    ctx!.stroke();
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
-   }
-});
-
-canvas.addEventListener("mouseup", () => {
-  cursor.active = false;
-});
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.cursor = "none";
+document.body.append(canvas);
