4a65e0e8900142f9b421dc8780126b3cee23390c
thuynh26 <thuynh26@ucsc.edu>
2025-10-17 00:55:06 +0000

    Co-authored-by: Ethan Akiyama <akiyamaethan@users.noreply.github.com>
    Setup canvas layout and simple drawing input

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>Heading</h1>
+  <canvas id="myCanvas" width="256" height="256"></canvas>
+
 `;
+
+const canvas = document.getElementById("myCanvas")! as HTMLCanvasElement;
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx!.beginPath();
+    ctx!.moveTo(cursor.x, cursor.y);
+    ctx!.lineTo(e.offsetX, e.offsetY);
+    ctx!.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+   }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
